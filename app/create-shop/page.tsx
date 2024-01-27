58e95b5e3a35c1dd959c0232cf3b00a921ca7af3
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2024-01-27 08:13:27 +0530

    Create shop UI

--- app/create-shop/page.tsx
+++ app/create-shop/page.tsx
@@ -1,9 +1,132 @@
-import React from 'react'
+"use client"
+
+import * as z from 'zod';
+import axios from 'axios';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import Link from 'next/link';
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormLabel,
+    FormMessage,
+    FormItem,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Loader2, Zap } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: "Shop name is required",
+    }),
+    location: z.string().min(1, {
+        message: "Shop location is required"
+    })
+});
 
 const CreateShopPage = () => {
-  return (
-    <div>CreateShopPage</div>
-  )
+    const router = useRouter();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            location: ""
+        },
+    });
+
+    const { isSubmitting, isValid } = form.formState;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        
+    }
+
+    return (
+        <div className='h-full mt-8 md:mt-20 mx-auto max-w-5xl p-4 flex md:items-center md:justify-center'>
+            <div>
+                <h1 className="text-2xl font-medium">
+                    Name your shop
+                </h1>
+                <p className="mt-1 text-sm text-slate-600">
+                    What would you like to name your shop? Emloyees can easily identify the shop
+                </p>
+
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className='space-y-8 mt-8'
+                    >
+                        <FormField
+                            control={form.control}
+                            name='name'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Shop name
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            disabled={isSubmitting}
+                                            placeholder="e.g. 'Dass Limited London'"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='location'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Location
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            disabled={isSubmitting}
+                                            placeholder="e.g. 'London, United Kingdom'"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <div className="flex items-center gap-x-2">
+                            <Link href="/">
+                                <Button
+                                    type="button"
+                                    variant='secondary'
+                                >
+                                    Cancel
+                                </Button>
+                            </Link>
+                            <Button
+                                type="submit"
+                                disabled={!isValid || isSubmitting}
+                                className={cn(
+                                    "flex gap-1 pr-5",
+                                    isSubmitting && "gap-2"
+                                )}
+                            >
+                                <Zap className={cn("w-5 h-5", isSubmitting && "hidden")} />
+                                <Loader2 className={cn("animate-spin w-5 h-5 hidden", isSubmitting && "flex")} />
+                                Create
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+            </div>
+        </div>
+    )
 }
 
 export default CreateShopPage
--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 
         <div className="w-full mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <Link href=''>
-            <div className="group flex flex-col px-6 py-5 gap-4 bg-slate-100 rounded-xl border hover:border-slate-400">
+            <div className="group flex flex-col p-5 md:px-6 md:py-5 gap-4 bg-slate-100 rounded-xl border hover:border-slate-400">
               <div className="flex flex-row justify-between items-center">
                 <h2 className="text-slate-800 text-lg font-medium">Shop Name</h2>
                 <ExternalLink className="w-5 h-5 text-slate-600 opacity-0 group-hover:opacity-100" />
