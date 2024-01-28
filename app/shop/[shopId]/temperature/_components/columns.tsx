a51d63d480b0633cd0367ecde66c7204473d5ece
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2024-01-28 16:50:42 +0530

    Add action btn

--- app/shop/[shopId]/temperature/_components/columns.tsx
+++ app/shop/[shopId]/temperature/_components/columns.tsx
@@ -1,7 +1,17 @@
 "use client"
 
-import { Temperature } from "@prisma/client"
-import { ColumnDef } from "@tanstack/react-table"
+import { Temperature } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal, Trash2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const columns: ColumnDef<Temperature>[] = [
   {
@@ -24,4 +34,31 @@ export const columns: ColumnDef<Temperature>[] = [
     accessorKey: "userName",
     header: "Employee",
   },
+  {
+    id: "actions",
+    header: "Action",
+    cell: ({ row }) => {
+      const { id } = row.original;        // temperature id
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-4 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(id)}
+              className="p-3"
+            >
+              <Trash2 className="h-4 w-4 mr-2" />
+              Copy ID
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    }
+  }
 ]
